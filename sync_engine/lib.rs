ded852c6ad3abcfb1de52e5cbd78ef205abd644c
vertigo <181855463+vertigo-z@users.noreply.github.com>
2026-06-11 06:58:15 +0000

    Delete sync_engine directory

--- sync_engine/lib.rs
+++
@@ -1,9 +0,0 @@
-mod change;
-mod engine;
-mod transport;
-mod ws;
-
-pub use change::{Change, ChangeOp};
-pub use engine::{SyncConfig, SyncEngine, SyncError, TableSpec};
-pub use transport::{SignedEnvelope, SyncMessage, Transport};
-pub use ws::WebSocketTransport;
